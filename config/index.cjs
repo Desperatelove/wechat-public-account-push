73517db9da3d5a450331e15fe4227fb090c37fbc
Desperatelove <132889610+Desperatelove@users.noreply.github.com>
2023-05-08 17:17:03 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o1OSb5sWFX7AukFh6if2VgO02x2w',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'IrW6mRRJ4StVppRXZb4hj5p-oyEqrZjLumMau3fDdxU',
+      useTemplateId: 'OQXoEgU0nLtkzAAdp6ZZOzjUaG6HN83hhxQgf3Tw7og',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-01',
       festivals: [
@@ -49,7 +49,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'IrW6mRRJ4StVppRXZb4hj5p-oyEqrZjLumMau3fDdxU',
+  CALLBACK_TEMPLATE_ID: 'OQXoEgU0nLtkzAAdp6ZZOzjUaG6HN83hhxQgf3Tw7og',
 
   CALLBACK_USERS: [
     {
